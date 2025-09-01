c0339e0642daf60856d63ec68918040fef6111b0
k.kuznetsov <k.kuznetsov@inlinegroup.ru>
2025-09-01 18:54:02 +0300

    first try

--- base_1.py
+++ base_1.py
@@ -1,5 +1,27 @@
 import pandas as pd
+import numpy as np
 
-df = pd.read_csv('')
+# df = pd.read_csv('')
+#
+# df.head()
 
-df.head()
+# matrix = [[56, 156],
+#           [70, 180],
+#           [45, 160]
+#           ]
+# matrix = np.array(matrix)
+# print(matrix)
+
+# создадим датафрейм из словаря
+# df = pd.DataFrame(
+#     {
+#     "Name": ["Braund, Mr. Owen Harris",
+#              "Allen, Mr. William Henry",
+#              "Bonnell, Miss. Elizabeth"],
+#     "Age": [22, 35, 58],
+#     "Sex": ["male", "male", "female"]
+#     }
+# )
+# print(df)
+#
+# print(df.describe())
